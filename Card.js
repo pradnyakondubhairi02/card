ffd5ab58e7aac704b3ee944d33b7f5aa70132e04
pradnya kondubhairi <91897968+pradnyakondubhairi02@users.noreply.github.com>
2023-01-01 22:04:19 +0530

    Add files via upload

---
+++ Card.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+export default function(){
+    return(
+    <div className="card">
+        <img src="prady.img.jpeg"/>
+        <h3>Pradnya Kondubhairi</h3>
+        <h5>Frontend Developer</h5>
+        <h6>pradnya.website</h6>
+
+      <div className="but">
+        <button className="mail">Email</button>
+        <button className="linkedin">Linkedin</button>
+        </div>
+
+     <div className="about">
+        <h3>About</h3>
+        <p>hii i am pradnya Kondubhairi hkhskvhjm,dn</p>
+    </div>
+
+    <div className="about">
+        <h3>interest</h3>
+        <p>hii i am pradnya Kondubhairi hkhskvhjm,dn</p>
+    </div>
+    </div>
+    )
+}
---
+++ index.js
@@ -0,0 +1,107 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// // import App from './App';
+// // import reportWebVitals from './reportWebVitals';
+
+
+// //   // <React.StrictMode>
+// //   //   <App />
+// //   // </React.StrictMode>
+// //   <h1>hii</h1>
+
+// function temp() {
+//   return(
+//   <div>
+//     <h1>Fun facts about React</h1>
+//     <ul>
+//       <li>Realeased in 2013</li>
+//       <li>is maintaineed by facebook</li>
+//       <li>100k strats on github</li>
+//     </ul>
+//   </div>
+//   // <div>
+//   //   <h1>My Awesome Website</h1>
+//   //   <h3>Reasons why i love react</h3>
+//   //   <ol>
+//   //     <li>Its compositive</li>
+//   //     <li>declmbj</li>
+//   //     <li>simple</li>
+//   //     <li>easy</li>
+//   //   </ol>
+//   // </div>
+// /* <nav>
+//     <h1>Prady's bistro</h1>
+//     <ul>
+//       <li>Menu</li>
+//       <li>About</li>
+//       <li>contact</li>
+//     </ul>
+//     </nav> */
+//   )
+
+//   }
+
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+// ReactDOM.render(temp(),document.getElementById("root"))
+
+// // ReactDOM.render()
+
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+// function Header() {
+//   return (
+//       <header>
+//           <nav>
+//               {/* <img src="./react-logo.png" width="40px" /> */}
+
+//               <ul className="nav-items">
+//                 <li>Pricing</li>
+//                 <li>About</li>
+//                 <li>contact</li>
+//               </ul>
+//           </nav>
+//       </header>
+//   )
+// }
+
+// function Footer() {
+//   return (
+//       <footer className="foot">
+//           <small>© 2021 Ziroll development. All rights reserved.</small>
+//       </footer>
+//   )
+// }
+
+// function MainContent() {
+//   return (
+//       <div>
+//           <h1>Reasons I'm excited to learn React</h1>
+//           <ol>
+//               <li>It's a popular library, so I'll be 
+//               able to fit in with the cool kids!</li>
+//               <li>I'm more likely to get a job as a developer
+//               if I know React</li>
+//           </ol>
+//       </div>
+//   )
+// }
+
+// function Page() {
+//   return (
+//       <div>
+//           <Header />
+//           <MainContent />
+//           <Footer />
+//       </div>
+//   )
+// }
+
+// ReactDOM.render(<Page />, document.getElementById("root"))
+
+import App from "./App"
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<App/>,document.getElementById("root"))
